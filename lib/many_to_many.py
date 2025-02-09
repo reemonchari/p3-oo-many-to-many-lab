0c04179772200bad5ebf520f2fc6adf0ce9f83e8
+ <+>
2025-02-09 14:11:08 +0300

    Many-to-Many Object Relationship

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,62 @@
 class Author:
-    pass
+    all = []
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise Exception("Name must be a string")
+        self.name = name
 
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+    
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+    
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts())
 
 class Book:
-    pass
+    all = []
+    def __init__(self, title):
+        if not isinstance(title, str):
+            raise Exception("Title must be a string")
+        self.title = title
+
+        Book.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+
+    def authors(self):
+        return list(set(contract.author for contract in self.contracts()))  # Ensures unique authors
 
 
 class Contract:
-    pass
+    all = []
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("Author must be an instance of Author")
+        if not isinstance(book, Book):
+            raise Exception("Book must be an instance of Book")
+        if not isinstance(date, str):
+            raise Exception("Date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("Royalties must be an integer")
+        
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        """Returns all contracts that match the given date."""
+        return [contract for contract in cls.all if contract.date == date]
+
+        
